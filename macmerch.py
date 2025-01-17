d628c64b5fdd89006a59cf4921ce4f9ac84ded8a
angiez37 <angelsaka@yahoo.com>
2025-01-17 11:02:31 -0500

    MVP completed (close 1)

--- macmerch.py
+++ macmerch.py
@@ -1,1 +1,17 @@
+import argparse
+def main():
+    #configure the first cli arg to be the price of the item
+    parser = argparse.ArgumentParser()
+    parser.add_argument("price", type=float, help="The price of the item")
+    args = parser.parse_args()
+    price = args.price
+
+    #add 13% tax
+    total = price * 1.13
+
+    print(f"The total price is: {total:.2f}")
+
+    if __name__ == "__main__":
+        main()
+
 
